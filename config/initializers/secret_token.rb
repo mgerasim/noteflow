a4d28541473095fc5dc7630b4266141f5b88c4a6
Mikahil Gerasimov <GerasimovMN@khv.dv.rt.ru>
2013-04-29 14:16:11 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noteflow::Application.config.secret_token = '439d8dc2a71555556ec632f6daf57ee0d41b121da0577f6d53776a877c2632659d66f4b54e87651a0ec6681c279439c0b40b3faa0468f9f567179cd4a95f674f'
